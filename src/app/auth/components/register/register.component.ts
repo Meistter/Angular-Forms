d45e73f9e2410f37b95934bc690e7e6be11b8c8d
Josue Perez <josueperez723@gmail.com>
2023-03-19 13:44:42 -0400

    runtime validations

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -39,9 +39,29 @@ export class RegisterComponent implements OnInit {
   private buildForm() {
     //Estamos usando el modulo validators, para usar aqui nuestra validacion personalizada
     this.form = this.formBuilder.group({
-      email: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6), MyValidators.validPassword]],
+      confirmPassword: ['',[Validators.required]],
+      type: ['company', [Validators.required]], //selecciona por defecto la opcion Company
+      companyName: ['',[Validators.required]],
+      range1: [],
+      range2: []
+    },{
+      //para validar que las contraseñas sean iguales debemos usar una validacion grupal, porq trata mas de 1 cammpo, eso lo hacemos asi:
+      validators: [MyValidators.equalPasswords, MyValidators.range]//!este viene a ser un error de todo el formulario, y que depende de si las contraseñas son iguales o no
     });
+
+    //*Esto es una validacion en tiempo de ejecucion
+    //?me subscribo al get del type para mantener control de cada vez que cambie
+    this.typeField.valueChanges.subscribe(value => {console.log(value)
+      if(value === 'company'){
+        this.companyNameField.setValidators([Validators.required])
+      }else{
+        this.companyNameField.setValidators(null)
+      }
+      this.companyNameField.updateValueAndValidity()
+      //! Con esta funcion asyncrona lo que estamos logrando es establecer validaciones en el radio group solamente cuando la opcion company este seleccionada
+    })
   }
 
   get emailField(){
@@ -49,6 +69,21 @@ export class RegisterComponent implements OnInit {
   }
   get passwordField(){
     return this.form.get('password')
+  }
+  get confirmPasswordField(){
+    return this.form.get('confirmPassword')
+  }
+  get range1Field(){
+    return this.form.get('range1')
+  }
+  get range2Field(){
+    return this.form.get('range2')
+  }
+  get typeField(){
+    return this.form.get('type')
+  }
+  get companyNameField(){
+    return this.form.get('companyName')
   }
 
 }
--- src/app/utils/validators.ts
+++ src/app/utils/validators.ts
@@ -19,6 +19,26 @@ export class MyValidators {
     return null
   }
 
+  static equalPasswords(control: AbstractControl){
+    const password = control.get('password').value //aqui estamos opteniendo todo el formulario en control, entonces seleccionamos a password especificamente
+    const confirmPassword = control.get('confirmPassword').value
+
+    if(password === confirmPassword){
+      return null //retornar nulo significa q no hay error
+    }
+    return {match_password: true} //este match_password es lo q usaremos para validar del otro lado
+
+  }
+  static range(control: AbstractControl){
+    const range1 = control.get('range1').value
+    const range2 = control.get('range2').value
+
+    if(range1 < range2 && range1 < 100 && range2 <= 100){
+      return null
+    }
+    return {range_right: true}
+  }
+
 }
 
 // Estas dos funciones me permiten validar si la contraseña recibida contiene al menos 1 numero
