3d56c90fd1fca7fd969cf706fa185326ff37f3f7
Josue Perez <josueperez723@gmail.com>
2023-03-27 19:52:18 -0400

    optimizacion de solicitudes al servidor debounceTime

--- src/app/home/components/search/search.component.ts
+++ src/app/home/components/search/search.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { query } from '@angular/animations';
-import { map } from 'rxjs/operators'
+import { map, debounceTime } from 'rxjs/operators'
 
+//!Este debounceTime nos permitira dar un tiempo para realizar el query de la busqueda, de forma que no busquemos con cada tecla q se presiona si no que se busque cuando el usuario haga una pausa de un tiempo determinado en su escritura, el tiempo lo determinamos nosotros normalmente 300ms
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -18,6 +19,7 @@ export class SearchComponent implements OnInit {
 
   ngOnInit(): void {
     this.searchField.valueChanges
+    .pipe(debounceTime(500)) //!Aqui definimos el tiempo que debe pasar despues que el usuario hace una pausa en su escritura para que ahora si envie el query
     .subscribe(value =>{
       this.getData(value)
     }) //aqui llamamos a ejecutar la busqueda cada vez q cambie el valor del input ya q le tenemos este observable o suscripcion activa
